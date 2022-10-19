e3f6e58149bb00c69fd962b195964bb05f93c149
gaoziheng <gaoziheng@bytedance.com>
2022-10-19 23:08:46 +0800

    feat: add first version for generate command

---
+++ example/config/plugin.default.ts
@@ -0,0 +1,8 @@
+import path from 'node:path'
+
+export default {
+  a: {
+    enable: true,
+    path: path.resolve(__dirname, '../plugin/a'),
+  },
+}
---
+++ example/framework/config/plugin.default.ts
@@ -0,0 +1,8 @@
+import path from 'node:path'
+
+export default {
+  b: {
+    enable: true,
+    path: path.resolve(__dirname, '../../plugin/b'),
+  },
+}
